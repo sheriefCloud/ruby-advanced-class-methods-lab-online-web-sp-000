c4c6e3db15d57a1f2be3943eebb27c37834b6531
Sherief Farid <sherief@farid.cloud>
2019-04-03 05:10:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     song
   end
 
-  # def self.find_by_name(name)
-  #   self.all.detect{|song| song.name = name}
-  # end
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name = name}
+  end
 
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
