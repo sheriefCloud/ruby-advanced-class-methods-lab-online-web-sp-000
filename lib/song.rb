e7c5a8f58387b6210bc737dec774cd3b5402a31e
Sherief Farid <sherief@farid.cloud>
2019-04-04 02:11:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
 
   def self.find_by_name(name)
     #binding.pry
-    @@all.detect{|song| song.name = name}
+    @@all.detect{|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
